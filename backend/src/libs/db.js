90554696b244418c8ab324075e98fc2c9f79f65e
Tên_Tài_Khoản_Mới <laitheanh1062006@gmail.com>
2026-08-13 11:25:05 +0700

    moji

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import dns from "dns";
 
 export const connectDB = async () => {
     try {
+        // Force Node resolver to use Google and Cloudflare DNS
+        dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
         await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING);
+
         console.log("Database connected");
     } catch (error) {
         console.log("error: ", error);
---
+++ frontend/src/components/chat/ChatWindowLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatWindowLayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChatWindowLayout
